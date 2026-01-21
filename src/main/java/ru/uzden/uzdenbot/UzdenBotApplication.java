a4be37f4aa4a29e3d0b0aa52fdd6064b1312ae79
rkxrichard <ibragimxasuev64@gmail.com>
2026-01-21 16:23:44 +0300

    начало

---
+++ src/main/java/ru/uzden/uzdenbot/UzdenBotApplication.java
@@ -0,0 +1,13 @@
+package ru.uzden.uzdenbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzdenBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzdenBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/uzden/uzdenbot/UzdenBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.uzden.uzdenbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzdenBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
