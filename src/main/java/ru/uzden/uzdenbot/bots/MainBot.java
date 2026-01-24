d33b88191cac91b2fa9cb845cc547e91cc957942
rkxrichard <ibragimxasuev64@gmail.com>
2026-01-24 13:56:17 +0300

    ботяра заработал)

--- src/main/java/ru/uzden/uzdenbot/bots/MainBot.java
+++ src/main/java/ru/uzden/uzdenbot/bots/MainBot.java
@@ -3,6 +3,7 @@ package ru.uzden.uzdenbot.bots;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
@@ -31,6 +32,11 @@ public class MainBot extends TelegramLongPollingBot {
         // обработка апдейтов
         if(update.hasMessage() && update.getMessage().hasText()) {
             System.out.println("Message: " + update.getMessage().getText());
+            try {
+                execute(SendMessage.builder().chatId(update.getMessage().getChatId()).text("Принял: " + update.getMessage().getText()).build());
+            }  catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
