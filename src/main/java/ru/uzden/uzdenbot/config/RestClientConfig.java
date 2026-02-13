1979b3823902ce4b04891029a471d2f28ae150b3
rkxrichard <ibragimxasuev64@gmail.com>
2026-02-13 16:29:25 +0300

    должно заработать

--- src/main/java/ru/uzden/uzdenbot/config/RestClientConfig.java
+++ src/main/java/ru/uzden/uzdenbot/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package ru.uzden.uzdenbot.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -11,5 +12,10 @@ public class RestClientConfig {
     @Bean
     public RestClient.Builder restClientBuilder() {
         return RestClient.builder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
